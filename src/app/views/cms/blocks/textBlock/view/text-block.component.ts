f545f4bc620ed9445e58c7b855e11996c9ad865f
mario <likearollingstone199@gmail.com>
2019-11-09 17:46:48 +0000

    Started working on text block removing

--- src/app/views/cms/blocks/textBlock/view/text-block.component.ts
+++ src/app/views/cms/blocks/textBlock/view/text-block.component.ts
@@ -28,6 +28,9 @@ export class TextBlockComponent implements AfterViewInit {
     });
   }
 
+  remove(internalName: string) {
+  }
+
   onUpdateWanted() {
     this.componentState.updateWanted = !this.componentState.updateWanted;
   }
